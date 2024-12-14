407a6c3af6efe1240443903780194c741b1f937c
Birkity <85987355+Birkity@users.noreply.github.com>
2024-12-14 20:18:02 +0300

    Replace FloatingActionButton with OutlinedButton in Result widget

--- lib/result.dart
+++ lib/result.dart
@@ -34,7 +34,7 @@ class Result extends StatelessWidget {
             ),
             textAlign: TextAlign.center,
           ),
-          FloatingActionButton(
+          OutlinedButton(
             onPressed: resetHandler,
             child: Text(
               'Reset Quiz',
@@ -42,7 +42,6 @@ class Result extends StatelessWidget {
                 color: Colors.white,
               ),
             ),
-            backgroundColor: Colors.blue,
           ),
         ],
       ),
