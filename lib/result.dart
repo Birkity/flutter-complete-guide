d983f5dbd9275d15a1b704784deb4ca9e07b4513
Birkity <85987355+Birkity@users.noreply.github.com>
2024-12-14 19:57:22 +0300

    Added conditional text to Result widget based on score

--- lib/result.dart
+++ lib/result.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
-  const Result({super.key});
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    var resultText = "You did it !";
+    if (resultScore <= 8) {
+      resultText = "You are awesome and great !";
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty Likeable';
+    } else if (resultScore <= 16) {
+      resultText = 'You are .... strange!';
+    } else {
+      resultText = 'You are not nice';
+    }
+
+    return resultText;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it !'),
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
     );
   }
 }
